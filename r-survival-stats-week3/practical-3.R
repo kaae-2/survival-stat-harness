a52e9e6dedfb8a67573ce465f12835ca82b0b3f1
Frederik <kaae-2@hotmail.com>
2026-03-09 20:15:27 +0100

    week 3 complete

--- r-survival-stats-week3/practical-3.R
+++ r-survival-stats-week3/practical-3.R
@@ -1,6 +1,7 @@
 # We consider the TRACE data that was collected by the TRACE study group, see for example Jensen, G.V., Torp-Pedersen, C., Hildebrandt, P., Kober, L., F. E. Nielsen, Melchior, T., Joen, T. and P. K. Andersen (1997), Does in-hospital ventricular fibrillation affect prognosis after myocardial infarction?, European Heart Journal 18, 919-924.
 
-# The data contains 1877 patients and is a subset of a data set consisting of approximately 6000 patients. It contains data relating survival of patients after myocardial infarction to various risk factors.
+# The data contains 1877 patients and is a subset of a data set consisting of approximately 6000 patients. 
+# It contains data relating survival of patients after myocardial infarction to various risk factors.
 
 #    time : observation time (years)
 #    status : status at exit (0: censored, 9: dead from myorcaridal, 8,7:other death )
@@ -13,14 +14,14 @@
 
 # To load the data
 
-# library(mets);
-# library(prodlim);
-# library(survival);
-# library(timereg)
+library(mets);
+library(prodlim);
+library(survival);
+library(timereg)
 # data(TRACE)
 
 # head(TRACE)
-
+  do 
 #        id wmi status chf    age sex diabetes  time vf
 # X6440 6440 1.9      9   0 66.114   0        0 6.237  0
 # X5201 5201 2.0      9   1 67.668   1        0 0.858  0
@@ -31,49 +32,50 @@
 
 # For the non-R users
 
-# ttt <- read.table("https://www.biostat.ku.dk/ts/survival/data/TRACE.txt")
-
+ttt <- read.table("https://www.biostat.ku.dk/ts/survival/data/TRACE.txt")
+head(ttt)
 # R commands for GOF
 
 # Some relevant R commands from the cheat-sheet
 
 # Cumulative residuals for proportionality
 
-# library(mets)
-# data(pbc); pbc=transform(pbc,lbili=log(bili),lpro=log(protime))
-# cox1 <- phreg(Surv(time,status==2)~age+edema+lbili+lpro+albumin,data=pbc)
-# gof1 <- gof(cox1)
+library(mets)
+data(pbc); pbc=transform(pbc,lbili=log(bili),lpro=log(protime))
+cox1 <- phreg(Surv(time,status==2)~age+edema+lbili+lpro+albumin,data=pbc)
+gof1 <- gof(cox1)
 
-# par(mfrow=c(2,3));
-# plot(gof1);
+
+par(mfrow=c(2,3));
+plot(gof1);
 
 # Cumulative residuals for linearity
 
-# pbc <- na.omit(pbc)
-# cox2 <- gofZ.phreg(Surv(time,status==2)~age+edema+lbili+lpro+albumin,data=pbc)
-# summary(cox2)
+pbc <- na.omit(pbc)
+cox2 <- gofZ.phreg(Surv(time,status==2)~age+edema+lbili+lpro+albumin,data=pbc)
+summary(cox2)
 
 # Timedependent Cox to describe effects
 
-# coxt <- coxph(Surv(time,status==2)~age+edema+lbili+lpro+albumin+tt(edema),data=pbc,
-#              tt=function(x,t,...) x*(t>1))
-# summary(coxt)
+coxt <- coxph(Surv(time,status==2)~age+edema+lbili+lpro+albumin+tt(edema),data=pbc,
+             tt=function(x,t,...) x*(t>1))
+summary(coxt)
 
 # The graphical old-school test
 
-# cox1 <- phreg(Surv(time,status==2)~age+strata(edema)+lbili+lpro+albumin,data=pbc)
-# bplot(cox1,log="y")
+cox1 <- phreg(Surv(time,status==2)~age+strata(edema)+lbili+lpro+albumin,data=pbc)
+bplot(cox1,log="y")
 
 # we can also make the time-axis on log-scale, but we are still cheking if lines are parallel l
 
-# cox1 <- phreg(Surv(time,status==2)~age+strata(edema)+lbili+lpro+albumin,data=pbc)
-# bplot(cox1,log="xy")
+cox1 <- phreg(Surv(time,status==2)~age+strata(edema)+lbili+lpro+albumin,data=pbc)
+bplot(cox1,log="xy")
 
 # Same plot (up to handling of ties) are done by
 
-# cox1 <- coxph(Surv(time,status==2)~age+strata(edema)+lbili+lpro+albumin,data=pbc)
-# ps <- survfit(cox1,data.frame(age=40,edema=c(0,0.5,1),lbili=0,lpro=0,albumin=0))
-# kmplot(ps,fun="cloglog",col=1:3)
+cox1 <- coxph(Surv(time,status==2)~age+strata(edema)+lbili+lpro+albumin,data=pbc)
+ps <- survfit(cox1,data.frame(age=40,edema=c(0,0.5,1),lbili=0,lpro=0,albumin=0))
+kmplot(ps,fun="cloglog",col=1:3)
 
 # Q1. GOF for Cox model
 
@@ -88,9 +90,23 @@
 
 # What do you conclude about the suitability of the Cox model.
 
+ttt$dead <- ttt$status!=0
+prop <- phreg(Surv(time, dead) ~vf+diabetes+age, data=ttt)
+propgof <- gof(prop)
+propgof
+
+ttt <- na.omit(ttt)
+lin <- gofZ.phreg(Surv(time, dead) ~vf+diabetes+age, data=ttt)
+summary(lin)
+
+par(mfrow=c(1,3))
+plot(propgof);
+
+# the conclusion is that diabetes and ventricular fibrillation is time dependent and the (simple) cox model is not the right tool
+
 # We shall not explore any possible correlations, again to keep life simple.
-# Q1. Improving the model
-# We look a cox model with vf, age and diabetes as predcitors.
+# Q2. Improving the model
+# We look a cox model with vf, age and diabetes as predictors.
 
 #    We can stratify on vf and examine the fit
 #        proportionality
@@ -99,15 +115,39 @@
 #        still need linearity
 #        we would cut time early
 #    Settle for conclusions at specific time points
-#        binomial regression (rmst analysis later)
+#        binomial regression (rmst analysis later) # did we learn this?
+
+prop <- phreg(Surv(time, dead) ~strata(vf)+diabetes+age, data=ttt)
+propgof <- gof(prop)
+propgof
+
+ttt <- na.omit(ttt)
+lin <- gofZ.phreg(Surv(time, dead) ~strata(vf)+diabetes+age, data=ttt)
+summary(lin)
+
+time <- coxph(Surv(time,dead) ~strata(vf)+age+diabetes+tt(diabetes), data=ttt, 
+              tt=function(x,t,...) x*(t>1))
+summary(time)
+
+
 
 # Q3. Describing the effect of vf using Cox models.
 
 # We shall now fit a Cox model with an early and a late effect. Looking at the survival plots or the cumulative residual plot you may guess on a useful cut-point for the early and late effects.
 
 # Try to express the results in a useful way.
+time <- coxph(Surv(time,dead) ~vf+age+tt(vf), data=ttt, 
+              tt=function(x,t,...) x*(t>0.0875))
+summary(time)
+
+# guessing 1 month
+
 # Q4. Survival Predictions
 # Using the simple cox model.
 
 #    Make predictions using the simple Cox model
 #    possibly also compare with binomial regression at specfic time points (not now).
+
+
+coxfinal <- coxph(Surv(time,dead) ~vf+age+diabetes, data=ttt)
+summary(coxfinal)
--- r-survival-stats-week3/prep-3.R
+++ r-survival-stats-week3/prep-3.R
@@ -19,52 +19,220 @@
 # asptr 	aspartate transaminase (IU/L)
 
 
-
+library(prodlim);
+library(survival);
+library(mets);
 # The purpose of the trial was to study the effect of treatment on the survival time. However, during the course of the trial an increased use of liver transplantation for patients with this disease made the investigators redefine the main response variable to be time to “failure of medical treatment” defined as either death or liver transplantation.
 
 
 # Load the data from the file pbc3subset.csv and store it in a data frame with the name pbc3 by the command
 
-# pbc3 <- read.csv2("http://biostat.ku.dk/frank/data/pbc3subset.csv")
+pbc3 <- read.csv2("http://biostat.ku.dk/frank/data/pbc3subset.csv")
 
-# We will study “treatment failure” defined as either death or liver transplantation. For this purpose make a new status variable which is 0 if the patient was still alive and without liver transplant when the study closed in 1989 and 1 if the patient died or had a liver transplant.
+# We will study “treatment failure” defined as either death or liver transplantation.
+# For this purpose make a new status variable which is 0 if the patient was still alive
+# and without liver transplant when the study closed in 1989 and 1 if the patient died or had a liver transplant.
 # Q1-1 Draw Kaplan-Meier curves to compare survival in the treatment group.
+
+pbc3$dead <- pbc3$status!=0
+outkm <- survfit(Surv(days, dead) ~tment, data= pbc3);
+
+kmplot(outkm)
+
+# look the same??
 
 # Q1-2A Fit a simple Cox model with treatment as explanatory variable to test if the CyA has
 # an effect on the outcome “treatment failure”. Is the CyA treatment effective?
+cox1 <- coxph(Surv(days, dead) ~tment, data=pbc3);
+summary(cox1)
+(1-0.91287)*100
+#based on this, can't say yes
 
 # Q1-3 Assess the proportional hazards assumption by the test and graphical procedure based
 # on the score process (cumulative martingale residual procedure for proportionality).
 
-# You can get the test for proportional hazards by fitting the Cox model with the phreg command from the mets package and then applying the gof function to the phreg object. You get the plots with the generic plot command, e.g., plot(gof.fit) where gof.fit is the result from the gof command.
-# PBC is a slowly progressing liver disease with patients diagnosed at varying disease stages, thereby making populations of patients with PBC rather heterogeneous. The liver function is not measured directly but only via biochemical markers such as bilirubin, albumin and aspartate transminase. From previous studies it is known that low albumin values and high levels of the other two markers indicate poor liver function.
+prop <- phreg(Surv(days, dead) ~tment, data=pbc3);
+gofprop <- gof(prop);
+plot(gofprop)
 
-# Q2-1A Consider how these variables affect survival by fitting simple unadjusted Cox models for each of them separately. Assess the functional form of the covariates (log-hazard linearity) by the method based on cumulative martingale residuals. Try also to log-transform each variable and assess the fit for the transformed variable.
+#it is proportional, because the p-value is above 0.05?
+# the plot also looks fine
 
-# To test for linearity (on the log-hazard scale) you can use command gofZ.phreg from the mets package. To get the plot, use the plot function with the extra argument type='z', i.e., use plot(gofZ.fit, type='z') where gofZ.fit is the result from the gofZ.phreg command.
+# You can get the test for proportional hazards by fitting the Cox model with
+# the phreg command from the mets package and then applying the gof function to the phreg object.
+# You get the plots with the generic plot command, e.g., plot(gof.fit)
+# where gof.fit is the result from the gof command.
+# PBC is a slowly progressing liver disease with patients diagnosed at varying disease stages,
+# thereby making populations of patients with PBC rather heterogeneous.
+# The liver function is not measured directly but only via biochemical markers such as bilirubin,
+# albumin and aspartate transminase. From previous studies
+# it is known that low albumin values and high levels of the other two markers indicate poor liver function.
+
+# Q2-1A Consider how these variables affect survival by fitting simple unadjusted Cox models for each of them separately.
+# Assess the functional form of the covariates (log-hazard linearity) by the method based on cumulative martingale residuals.
+# Try also to log-transform each variable and assess the fit for the transformed variable.
+
+pbc3 = transform(pbc3, lalb=log(alb), lbili=log(bili), lasptr=log(asptr));
+cox1 <- phreg(Surv(days, dead) ~tment+alb+lalb+bili+lbili+asptr+lasptr, data=pbc3);
+gvf <- gof(cox1);
+par(mfrow=c(3,3))
+plot(gvf)
+summary(gvf);
+
+vars <- c("tment","alb","lalb","bili","lbili","asptr","lasptr")
+
+par(mfrow = c(3,3))   # one plot at a time
+
+for(v in vars){
+
+  form <- as.formula(paste("Surv(days, dead) ~", v))
+
+  fit <- phreg(form, data = pbc3)
+
+  gvf <- gof(fit)
+
+  plot(gvf, main = paste("Cumulative score process:", v))
+
+  print(summary(gvf))
+}
+
+# just bili?
+
+# To test for linearity (on the log-hazard scale) you can use command gofZ.phreg from the mets package.
+# To get the plot, use the plot function with the extra argument type='z', i.e., use plot(gofZ.fit, type='z')
+# where gofZ.fit is the result from the gofZ.phreg command.
+
+vars <- c("alb","lalb","bili","lbili","asptr","lasptr")
+
+par(mfrow = c(3,3))   # one plot at a time
+
+for(v in vars){
+
+  form <- as.formula(paste("Surv(days, dead) ~", v))
+
+  fit <- tryCatch(
+    gofZ.phreg(form, data = pbc3),
+    error = function(e) {
+      message("Skipping ", v, ": ", e$message)
+      return(NULL)
+    }
+  )
+  if (is.null(fit)) next
+
+
+  plot(fit, type='z')
+
+  print(summary(fit))
+}
 
 # Based on the p-values of the tests found under “Cumulative residuals versus model matrix”, which variables (raw and log-transformed) are rejected at the 5% significance level?
 
+# bili and asptr
+# apparently its only bili
+
+
 # Q2-2 Assess the proportional hazards assumption for the functional form you find suitable in question Q2-1A
+
+par(mfrow=c(2,3))
+cox_final <- phreg(Surv(days, dead) ~ tment +lalb + lbili + lasptr, data = pbc3)
+gof_final <- gof(cox_final)
+plot(gof_final)
+summary(gof_final)
+
+# the proportional assumption holds
 
 # Q2-3A Do your results verify that a poor liver function at randomization yields a higher hazard for “treatment failure”?
 
-# Because this trial was randomized, the liver function should be balanced in the CyA and placebo groups. Thus, the simple analysis in Q1 including only treatment could be considered the “correct” analysis and end
+# fit <- phreg(Surv(days, dead) ~ lalb + lbili + lasptr, data = pbc3)
+fit <- coxph(Surv(days, dead) ~ lalb + lbili + lasptr, data = pbc3)
+s <- summary(fit)
+s
+# apparently all of them have hazard 
+
+# hazard ratios with CI
+HR  <- exp(coef(fit))
+CI  <- exp(confint(fit))
+cbind(HR, CI)
+
+# the albumin and bilirubin are significant.
+
+# Because this trial was randomized, the liver function should be balanced in the CyA and placebo groups.
+# Thus, the simple analysis in Q1 including only treatment could be considered the “correct” analysis and end
 # of the story.
 
-# It should however be kept in mind that randomization only ensures complete balancing of treatment groups for very large trials. You will investigate if patients in one of the treatment groups is more severely ill than those in the other group, despite the randomization.
+# It should however be kept in mind that randomization only ensures complete balancing of treatment groups for very large trials.
+# You will investigate if patients in one of the treatment groups is more severely ill than those in the other group, despite the randomization.
 
-# Q3-1 For each of the three biomarkers bilirubin, albumin and aspartate transminase draw box plots for the CyA treatment and placebo treated groups. Do the distributions appear to be similar? Which of the treatment groups has the worst liver function?
+# Q3-1 For each of the three biomarkers bilirubin, albumin and aspartate transminase
+# draw box plots for the CyA treatment and placebo treated groups.
+# Do the distributions appear to be similar? Which of the treatment groups has the worst liver function?
 
-# Q3-2A Calculate the mean and standard deviation for albumin, log2-transformed bilirubin and log2-transformed aspartate transminase for the CyA treatment and placebo treated groups. Which of the treatment groups has the worst liver function?
+par(mfrow = c(2,3))
 
-# Q3-3 It is the size of the difference in the markers (in absolute terms) between the groups that matters. Because the study was randomized, it does not make sense to make statistical significance tests to compare the groups. Why?
+boxplot(bili ~ tment, data = pbc3,
+        main = "Bilirubin by treatment",
+        xlab = "Treatment",
+        ylab = "Bilirubin")
+
+boxplot(alb ~ tment, data = pbc3,
+        main = "Albumin by treatment",
+        xlab = "Treatment",
+        ylab = "Albumin")
+
+boxplot(asptr ~ tment, data = pbc3,
+        main = "AST by treatment",
+        xlab = "Treatment",
+        ylab = "AST")
+
+boxplot(lbili ~ tment, data = pbc3, main="log(Bilirubin)")
+boxplot(lalb ~ tment, data = pbc3, main="log(Albumin)")
+boxplot(lasptr ~ tment, data = pbc3, main="log(AS)")
+
+# i would say they are close, maybe log albumin and bilirubin
+
+# Q3-2A Calculate the mean and standard deviation for albumin, log2-transformed bilirubin
+# and log2-transformed aspartate transminase for the CyA treatment and placebo treated groups.
+# Which of the treatment groups has the worst liver function?
+
+tapply(pbc3$alb, pbc3$tment,
+       function(x) c(mean=mean(x,na.rm=TRUE), sd=sd(x,na.rm=TRUE)))
+
+tapply(pbc3$lbili, pbc3$tment,
+       function(x) c(mean=mean(x,na.rm=TRUE), sd=sd(x,na.rm=TRUE)))
+
+tapply(pbc3$lasptr, pbc3$tment,
+       function(x) c(mean=mean(x,na.rm=TRUE), sd=sd(x,na.rm=TRUE)))
+
+# the treatment group with the low albumin has the worst function?
+
+# Q3-3 It is the size of the difference in the markers (in absolute terms) between the groups that matters.
+# Because the study was randomized, it does not make sense to make statistical significance tests to compare the groups. Why?
+# With the findings from the previous steps we fit a multiple Cox model for treatment
+# adjusting for the three liver function markers.
+# No interactions were considered of special interest in this study,
+# so we don’t pursue a study of interactions between treatment and other variables.
+
+coxtrt <- coxph(Surv(days, dead) ~ tment + alb + bili + asptr, data = pbc3)
+summary(coxtrt)
+
+# it does not make sense to compare the groups because they are the same? that's the whole point of randomizing?
+#
+#
 
 
-# With the findings from the previous steps we fit a multiple Cox model for treatment adjusting for the three liver function markers. No interactions were considered of special interest in this study, so we don’t pursue a study of interactions between treatment and other variables.
+# Q4-1. Fit a Cox model with treatment, albumin, log2(bilirubin) and log2(aspartate transaminase)
+# as explanatory variables. Check the model assumptions. Interpret the results.
 
-# Q4-1. Fit a Cox model with treatment, albumin, log2(bilirubin) and log2(aspartate transaminase) as explanatory variables. Check the model assumptions. Interpret the results.
+coxltrt <- coxph(Surv(days, dead) ~ tment + alb + lbili + lasptr, data = pbc3)
+summary(coxltrt)
+
+# treatment halves hazard significantly, doubling the bili 2.5x the risk, higher albumin lowers the hazard by 10%
+# lasptr is not significant
 
 # Q4-2A Compare your adjusted model to the unadjusted model from Q1. Is the “message” on
 # the potential effect of the treatment different? Given the randomization, which of the models
 # do you prefer?
+
+# with the simple model, treatment seems to have no significant effect.
+# Adjusting for the different proteins, we do find a significant value of the treatment
+# I would prefer the adjusted model, as we're trying to isolate the effect of treatment

48ff20b193e92442068f36a838f3af51da3a44f5
Frederik <kaae-2@hotmail.com>
2026-03-18 16:08:03 +0100

    finished week 5

--- r-survival-stats-week5/practical-5.R
+++ r-survival-stats-week5/practical-5.R
@@ -20,13 +20,14 @@
 
 # The aim of the exercises are to familiarize you with how to estimate the cumulative incidence and how to regression for the cumulative incidence. We shall thus focus on the competing risks aspect of the data.
 
-# library(riskRegression)
-# library(prodlim)
-# library(mets)
-# library(cmprsk)
-# data(Melanoma,package="riskRegression")
-# Melanoma$logThick=log(Melanoma$thick)
-# dtable(Melanoma,~status)
+library(riskRegression)
+library(prodlim)
+library(mets)
+library(cmprsk)
+data(Melanoma,package="riskRegression")
+Melanoma$logThick=log(Melanoma$thick)
+Melanoma$ulcer <- factor(Melanoma$ulcer)
+dtable(Melanoma,~status)
 
 # We note that the status variable has the censorings scored as "0" which is what we want !
 
@@ -60,55 +61,63 @@
 # R commands for today
 
 # Some relevant R commands from the cheat-sheet
-
-# Product limit
-
-# cifu=prodlim(Hist(time,status)~factor(ulcer),data=Melanoma)
+# 
+# # Product limit
+# 
+# cifu=prodlim(Hist(time,status)~ulcer,data=Melanoma)
 # plot(cifu)
 # summary(cifu,times=1000)
-
-# Fine-Gray
-
-# fitFG1=cifreg(Event(time,status)~factor(ulcer),data=Melanoma,cause=1,prop=NULL)
-# fitFG2=cifreg(Event(time,status)~factor(ulcer),data=Melanoma,cause=2,prop=NULL)
-
-# ## predicting based on data.frame
-# nd <- data.frame(ulcer=c(1,2))
-# pfg1 <- predict(fitFG1,nd)
-# pfg2 <- predict(fitFG2,nd)
-
-# ## plotting
-# par(mfrow=c(1,2))
+# 
+# # Fine-Gray
+# 
+# 
+# fitFG1 <- cifregFG(Event(time, status) ~ ulcer, data = Melanoma, cause = 1)
+# fitFG2 <- cifregFG(Event(time, status) ~ ulcer, data = Melanoma, cause = 2)
+# 
+# nd <- data.frame(
+#   ulcer = factor(c("not present", "present"),
+#                  levels = levels(Melanoma$ulcer))
+# )
+# 
+# pfg1 <- predict(fitFG1, newdata = nd)
+# pfg2 <- predict(fitFG2, newdata = nd)
+# 
+# par(mfrow = c(1,2))
 # plot(pfg1)
 # plot(pfg2)
-
-# OR-version of CIF regression
-
-# fitOR1=cifreg(Event(time,status)~factor(ulcer),data=Melanoma,cause=1)
-# fitOR2=cifreg(Event(time,status)~factor(ulcer),data=Melanoma,cause=2)
-
-# ## predicting based on data.frame
+# # OR-version of CIF regression
+# 
+# fitOR1=cifreg(Event(time,status)~ulcer,data=Melanoma,cause=1)
+# fitOR2=cifreg(Event(time,status)~ulcer,data=Melanoma,cause=2)
+# 
+# # ## predicting based on data.frame
 # nd <- data.frame(ulcer=c(1,2))
 # por1 <- predict(fitOR1,nd)
 # por2 <- predict(fitOR2,nd)
-
+# 
 # par(mfrow=c(1,2))
 # plot(por1)
 # plot(por2)
-
-# Based on cause-specific hazards modelling
-
+# 
+# # Based on cause-specific hazards modelling
+# 
 # library(riskRegression); library(pec)
-# fit2 <- CSC(Hist(time,status)~ulcer,data=Melanoma)
-# nd=data.frame(ulcer=c(1,2))
-# times <- seq(10,4000,by=100)
-# plotPredictEventProb(fit2,newdata=nd,times,cause=1,col=1:3)
-
-# If you can install the package on your own, requires some additional work that you may skip, but is my preferred method. GOF (cumulative residuals) for "proportionality"
-
-# #library(crskdiag)
-# #out1 <- diag_crr(Crsk(time,status)~ulcer,data=Melanoma,test="prop",seed=1234)
-# #print(out1)
+# fit2 <- CSC(Hist(time,status) ~ ulcer, data=Melanoma)
+# 
+# nd <- data.frame(
+#   ulcer = factor(c("not present","present"),
+#                  levels = levels(Melanoma$ulcer))
+# )
+# 
+# times <- seq(10, 4000, by = 100)
+# par(mfrow=c(1,1))
+# plotPredictEventProb(fit2, newdata = nd, times = times, cause = 1, col = 1:2)
+# 
+# # If you can install the package on your own, requires some additional work that you may skip, but is my preferred method. GOF (cumulative residuals) for "proportionality"
+# 
+# # library(crskdiag)
+# # out1 <- diag_crr(Crsk(time,status)~ulcer,data=Melanoma,test="prop",seed=1234)
+# # print(out1)
 # source("https://www.biostat.ku.dk/ts/survival/data/gofFG.R")
 # gg <- gofFG(Event(time,status)~ulcer,data=Melanoma)
 # summary(gg)
@@ -125,8 +134,53 @@
 
 # Q1. Non-parametric Cumulative Incidence
 # Estimate the cumulative incidence for the two causes, do plots and list the estimates with 95% confidence intervals at 2000 days.
+fit.ci=prodlim(Hist(time,status)~1,data=Melanoma)
+
+# plots
+par(mfrow=c(1,2))
+plot(fit.ci, cause = 1, xlab = "Days", ylab = "Cumulative incidence",
+     main = "Cause 1: death from melanoma")
+plot(fit.ci, cause = 2, xlab = "Days", ylab = "Cumulative incidence",
+     main = "Cause 2: death from other causes")
+
+# estimates with 95% CI at 2000 days
+ci.cause1 <- summary(fit.ci, times = 2000, cause = 1)
+ci.cause2 <- summary(fit.ci, times = 2000, cause = 2)
+
+ci.cause1
+ci.cause2
+
 # Q2 Stratified after invasion
-# Plot the Aalen Johansen estimates of the cumulative incidence functions stratified by the levels of the covariate invasion. Do a Gray's test for equivalence for the cumulative incidence curves.
+# Plot the Aalen Johansen estimates of the cumulative incidence functions 
+# stratified by the levels of the covariate invasion. 
+# Do a Gray's test for equivalence for the cumulative incidence curves.
+
+# make invasion a factor for grouped curves
+Melanoma$invasion <- factor(Melanoma$invasion)
+
+# Aalen-Johansen cumulative incidence curves stratified by invasion
+fit.inv <- prodlim(Hist(time, status) ~ invasion, data = Melanoma)
+
+par(mfrow = c(1,2))
+plot(fit.inv, cause = 1,
+     xlab = "Days", ylab = "Cumulative incidence",
+     main = "Cause 1 by invasion")
+
+plot(fit.inv, cause = 2,
+     xlab = "Days", ylab = "Cumulative incidence",
+     main = "Cause 2 by invasion")
+
+# Gray's test
+gray.inv <- cuminc(
+  ftime = Melanoma$time,
+  fstatus = Melanoma$status,
+  group = Melanoma$invasion,
+  cencode = 0
+)
+
+gray.inv
+
+
 # Q3 CIF regression, FG-Model
 
 #    Fit a Fine-Gray Model cumulative incidence model for the covariate invasion for the two cumulative incidence functions.
@@ -135,44 +189,124 @@
 
 # R-Hint: to get the data frame with the invasion levels for the predictions
 
-# nd=data.frame(invasion=levels(Melanoma$invasion))
+nd=data.frame(invasion=levels(Melanoma$invasion))
 
+fitFG1=cifreg(Event(time,status)~invasion,data=Melanoma,prop=NULL,cause=1)
+fitFG2=cifreg(Event(time,status)~invasion,data=Melanoma,prop=NULL,cause=2)
+
+nd=data.frame(invasion=levels(Melanoma$invasion))
+
+par(mfrow=c(1,2))
+pfit1=predict(fitFG1,nd)
+pfit2=predict(fitFG2,nd)
+
+plot(pfit1,col=1:3)
+plot(fit1,add=TRUE,confint=FALSE)
+legend("topleft",legend=c("level.0","level.1","level.2"),lty=1:3,col=1:3)
+plot(pfit2)
+plot(fit1,cause=2,add=TRUE,confint=FALSE)
+legend("topleft",legend=c("level.0","level.1","level.2"),lty=1:3,col=1:3)
+
+summary(fitFG1)
+summary(fitFG2)
+
+out1 <- gofFG(Event(time,status)~invasion,data=Melanoma)
+summary(out1)
 # Q4 CIF regression, logistic-link OR-Model
-# Fit the logistic-link OR cumulative incidence model for the covariate invasion for the two cumulative incidence functions. Make predictions and compare to the non-parametric estimates.
+# Fit the logistic-link OR cumulative incidence model for the covariate invasion for the two cumulative incidence functions. 
+# Make predictions and compare to the non-parametric estimates.
+
+fitOR1=cifreg(Event(time,status)~invasion,data=Melanoma,cause=1)
+fitOR2=cifreg(Event(time,status)~invasion,data=Melanoma,cause=2)
+
+nd=data.frame(invasion=levels(Melanoma$invasion))
+
+par(mfrow=c(1,2))
+pfit1=predict(fitOR1,nd)
+pfit2=predict(fitOR2,nd)
+
+plot(pfit1,col=1:3)
+legend("topleft",legend=c("level.0","level.1","level.2"),lty=1:3,col=1:3)
+plot(pfit2)
+legend("topleft",legend=c("level.0","level.1","level.2"),lty=1:3,col=1:3)
+
+summary(fitOR1)
+summary(fitOR2)
+
+library(timereg)
+gofOR1=prop.odds.subdist(Event(time,status)~invasion,data=Melanoma,cause=1)
+gofOR2=prop.odds.subdist(Event(time,status)~invasion,data=Melanoma,cause=2)
+summary(gofOR1)
+summary(gofOR2)
+
+
 # Q4G
 # If you have time you may also do a check that the censoring distribution does not depend on the covariates.
+cc <-coxph(Surv(time,status==0)~invasion,data=Melanoma)
+summary(cc)
+
 # Q5 Fixed time-horizon IPCW analysis
-# Still looking only at invasion-level make a competing risks analysis that describes the risk of dying from malignant melanoma before 2000 days (1000 days) using the binomial regression binreg-function.
+# Still looking only at invasion-level make a competing risks analysis that describes 
+# the risk of dying from malignant melanoma before 2000 days (1000 days) using the binomial regression binreg-function.
+cc2000 <-binreg(Event(time,status)~invasion,data=Melanoma,cause=1,time=2000)
+cc1000 <-binreg(Event(time,status)~invasion,data=Melanoma,cause=1,time=1000)
+summary(cc2000)
+summary(cc1000)
+
 # Q6 Cause Specific Hazards
-
 # If time allows you might also compare to the predictions obtained starting instead with the cause-specific hazards. But this may be skipped.
-
 # Fit a univariate model with the covariate invasion for the 2 cause specific hazard functions and plot the cumulative incidence functions for the 3 levels of invasion based on the cause-specific hazards.
 #
-# CIF-2-Practicals
+library(pec)
+fit2 <- CSC(Hist(time,status)~invasion,data=Melanoma)
+par(mfrow=c(1,2))
+nd=data.frame(invasion=levels(Melanoma$invasion))
+times=seq(0,5000,by=100)
+plotPredictEventProb(fit2,newdata=nd,times,cause=1,col=1:3)
+legend("topleft",legend=c("level.0","level.1","level.2"),lty=1,col=1:3)
+plotPredictEventProb(fit2,newdata=nd,times,cause=2,col=1:3)
+legend("topleft",legend=c("level.0","level.1","level.2"),lty=1,col=1:3)
+
+fit2 <- CSC(Hist(time,status)~invasion,data=Melanoma)
+print(fit2)
+
+cause1=predictEventProb(fit2,newdata=data.frame(invasion="level.0"),cause=1,times=fit$time)
+cause2=predictEventProb(fit2,newdata=data.frame(invasion="level.0"),cause=2,times=fit$time)
+Melanoma$survStatus=ifelse(Melanoma$status>0,1,0)
+fitSurv=coxph(Surv(time,survStatus)~invasion,data=Melanoma,x=TRUE)
+survProb=predictSurvProb(fitSurv,newdata=data.frame(invasion="level.0"),times=fit$time)
+plot(fit$time,cause1,type="s",ylim=c(0,1),col="red",ylab="CIF",xlab="Time In Days")
+lines(fit$time,cause1+cause2,col="blue",type="s")
+lines(fit$time,survProb,col="magenta",type="s")
+legend("topright", legend=c("CIF Cause 1","CIF Cause 2+1","Survival Prob"),
+       col=c("red","blue","magenta"),lty=1)
+
+  
+## CIF-2-Practicals
 # Table of Contents
 
 # We shall now consider multivariate cumulative incidence regression modelling.
 
-# library(riskRegression)
-# library(survival)
-# library(cmprsk)
-# library(pec)
-# library(mets)
-# data(Melanoma,package="riskRegression")
-# Melanoma$logThick=log(Melanoma$thick)
+library(riskRegression)
+library(survival)
+library(cmprsk)
+library(pec)
+library(mets)
+data(Melanoma,package="riskRegression")
+Melanoma$logThick=log(Melanoma$thick)
 
 # To simplify the R-aspects of the below we code all factors with two levels as a numeric variable and work with some of these in various places below
 
-# dnumeric(Melanoma) <- ~.
-# dtable(Melanoma,~ulcer+ulcer.n)
-# dtable(Melanoma,~epicel+epicel.n)
-# dtable(Melanoma,~sex+sex.n)
-# dtable(Melanoma,~invasion+invasion.n)
-# dtable(Melanoma,~event+event.n)
-# Melanoma=transform(Melanoma,ulcer=ulcer.n,epicel=epicel.n,sex=sex.n)
+dnumeric(Melanoma) <- ~.
+dtable(Melanoma,~ulcer+ulcer.n)
+dtable(Melanoma,~epicel+epicel.n)
+dtable(Melanoma,~sex+sex.n)
+dtable(Melanoma,~invasion+invasion.n)
+dtable(Melanoma,~event+event.n)
+Melanoma=transform(Melanoma,ulcer=ulcer.n,epicel=epicel.n,sex=sex.n)
 
-# It is important that the censorings are coded as "0" for the below analyses. This is asssumed by most programs, although a different code can also be specified in the programs.
+# It is important that the censorings are coded as "0" for the below analyses. 
+# This is asssumed by most programs, although a different code can also be specified in the programs.
 # Q6 Multivariate Modelling
 
 # Do a Fine-Gray model with the covariates: sex,epicel,ulcer,age,logThick and invasion.
@@ -190,14 +324,76 @@
 
 #     sex=1,epicel=1,ulcer=1,age=50,logThick=1.
 
+
+fitFG1=cifreg(Event(time,status)~sex+epicel+ulcer+age+logThick+invasion,data=Melanoma,cause=1,prop=NULL)
+fitFG2=cifreg(Event(time,status)~sex+epicel+ulcer+age+logThick+invasion,data=Melanoma,cause=2,prop=NULL)
+
+## newdata for predictions
+nd=data.frame(invasion=levels(Melanoma$invasion),sex=1,epicel=1,ulcer=1,age=50,logThick=1)
+
+par(mfrow=c(1,2))
+pfit1=predict(fitFG1,nd)
+pfit2=predict(fitFG2,nd)
+
+plot(pfit1,col=1:3)
+legend("topleft",legend=c("ADJ-level.0","ADJ-level.1","ADJ-level.2"),lty=1:3,col=1:3)
+plot(pfit2)
+legend("topleft",legend=c("ADJ-level.0","ADJ-level.1","ADJ-level.2"),lty=1:3,col=1:3)
+summary(fitFG1)
+summary(fitFG2)
+exp(estimate(coef=fitFG1$coef,vcov=fitFG1$var)$coefmat[,c(1,3,4)])
+
+s1fitFG1=cifreg(Event(time,status)~strata(sex)+epicel+ulcer+age+logThick+invasion,data=Melanoma,cause=1,propodds=NULL)
+s2fitFG1=cifreg(Event(time,status)~sex+epicel+ulcer+age+logThick+strata(invasion),data=Melanoma,cause=1,propodds=NULL)
+
+par(mfrow=c(1,2))
+plot(s1fitFG1,log="y"); title(main="sex"); 
+plot(s2fitFG1,log="y"); title(main="Invasion");
 # Q7 Multivariate Modelling OR-modelling
-# Do a logistic regression model for the CIF's. Again you may also get around to checking the Goodness of fit of the model, and the predictions.
-# Q8 Multivariate Modelling, censoring assumption
-# Check if the censorings are independent of the covariates by doing a Cox model for the censorings. Specifically we fit a Cox regression model for the censorings.
+# Do a logistic regression model for the CIF's. Again you may also get around 
+# to checking the Goodness of fit of the model, and the predictions.
+
+fitOR1=cifreg(Event(time,status)~sex+epicel+ulcer+age+logThick+invasion,data=Melanoma,cause=1)
+fitOR2=cifreg(Event(time,status)~sex+epicel+ulcer+age+logThick+invasion,data=Melanoma,cause=2)
+
+nd=data.frame(invasion=levels(Melanoma$invasion),sex=1,epicel=1,ulcer=1,age=50,logThick=1)
+
+par(mfrow=c(1,2))
+pfit1=predict(fitOR1,nd)
+pfit2=predict(fitOR2,nd)
+
+plot(pfit1,col=1:3)
+legend("topleft",legend=c("ADJ-level.0","ADJ-level.1","ADJ-level.2"),lty=1:3,col=1:3)
+plot(pfit2)
+legend("topleft",legend=c("ADJ-level.0","ADJ-level.1","ADJ-level.2"),lty=1:3,col=1:3)
+
+summary(fitOR1)
+summary(fitOR2)
+
+library(timereg)
+gofOR1=prop.odds.subdist(Event(time,status)~sex+epicel+ulcer+age+logThick+invasion,data=Melanoma,cause=1)
+gofOR2=prop.odds.subdist(Event(time,status)~sex+epicel+ulcer+age+logThick+invasion,data=Melanoma,cause=2)
+summary(gofOR1)
+summary(gofOR2)
+ # Q8 Multivariate Modelling, censoring assumption
+# Check if the censorings are independent of the covariates by doing a Cox model for the censorings. 
+# Specifically we fit a Cox regression model for the censorings.
+
+censModel=coxph(Surv(time,status==0)~sex+ulcer+epicel+
+                  logThick+age,data=Melanoma)
+summary(censModel)
+
 # Q9. Binomial-Regression: Modelling just one time-point
-
-# When there are model fit issues as we have seen above it can be useful to consider just specific time-points such as the cumulative incidence at 2000 days, for example. Then we may fit the binomial regression model with censoring adjustment.
-
+# When there are model fit issues as we have seen above it can be useful to consider 
+# just specific time-points such as the cumulative incidence at 2000 days, for example. 
+# Then we may fit the binomial regression model with censoring adjustment.
 # Do a binomial regression for cause 1 at 2000 days.
-
 # Here we also rely on the assumption that the censorings do not depend on the covariates.
+
+fitOR1t=binreg(Event(time,status)~sex+epicel+ulcer+age+logThick+invasion,data=Melanoma,cause=1,time=2000)
+summary(fitOR1t)
+dcut(Melanoma,breaks=2) <- ~logThick+age
+fitOR1tc=binreg(Event(time,status)~sex+epicel+ulcer+age+logThick+invasion,data=Melanoma,
+                cause=1,time=2000,cens.model=~strata(logThickcat.2,agecat.2))
+summary(fitOR1tc)
+
--- r-survival-stats-week5/prep-5.R
+++ r-survival-stats-week5/prep-5.R
@@ -1,2 +1,139 @@
-# Prep exercises for this week
-# TODO: add your prep tasks here.
+# Stanford heart transplant study
+# 
+# Crowley and Hu (J. Amer. Statist. Assoc., 1977) describe a program at Stanford University where 103 patients were registered to recieve a heart transplant and were followed until death or censorship. Of these, 65 received transplants during follow-up, whereas 38 did not
+# 
+# Question: Does getting a heart transplant improve survival?
+#   
+#   The variables in the data set are
+# age 	    Age in years at entry into the study
+# death 	  0=censored
+#           1=dead
+# days 	    The follow-up time, days from entry to death/censoring
+# trans 	  0=no heart transplant
+#           1=heart transplant
+# wait 	    Days from entry to transplant
+#           If trans=0 then wait is NA
+# mismatch 	0=Human leukocyte antigen (HLA) type in donor and patient match
+#           1=HLA mismatch
+#           If trans=0 then mismatch is NA
+# 
+# Load the data into R and inspect the six first records
+library(prodlim);
+library(survival); 
+library(mets);
+
+
+heart <- read.csv2("http://biostat.ku.dk/frank/data/stanford.csv")
+head(heart)
+
+# Q1-1. Plot Kaplan-Meier curves treating transplantation as a time-fixed covariate 
+# and fit a simple Cox regression model where you “evaluate the effect of transplantation” 
+# using transplantation as a time-fixed covariate. How should this analysis be interpreted? 
+# Think carefully in terms of the information provided by the covariate trans. 
+# Use follow-up time as time axis in the Cox model and adjust for age at entry.
+ss=prodlim(Surv(days,death) ~trans+age,data=heart)
+plot(ss);
+
+out=coxph(Surv(days,death) ~ trans+age,data=heart)
+newdata=data.frame(trans=c(0,1)); 
+pred=survfit(out,newdata); 
+plot(pred);
+
+plot(pred, col = 1:2, lty = 1:2,
+     xlab = "Follow-up time",
+     ylab = "Survival probability")
+legend("bottomleft", legend = levels(factor(heart$trans)),
+       col = 1:2, lty = 1:2)
+exp(coef(out))
+exp(confint(out))
+
+# This comparison should not be interpreted as the causal effect of transplantation, 
+# because the transplanted group is defined using future information and necessarily excludes early deaths before transplantation. 
+# Thus, the analysis is affected by immortal time bias and will typically overstate the benefit of transplantation.
+# Transplantation should be analyzed as a time-dependent covariate in a Cox model. Then patients contribute person-time as:
+# - non-transplanted before transplant
+# - transplanted after transplant
+
+
+# Q1-2A. Make a landmark analysis at time 30 days on the waiting list. 
+# That is, consider only patients still alive and under observation after 30 days, 
+# and use this landmark time as time origin (“time zero”). 
+# How many patients are there in each group at the (new) baseline? 
+#   How many were excluded because they were already dead by day 30? 
+#   How many were excluded because they were censored?
+
+landmark <- 30
+
+lmdata <- subset(heart, days > landmark)
+
+excluded_dead <- subset(heart, days <= landmark & death == 1)
+excluded_cens <- subset(heart, days <= landmark & death == 0)
+
+nrow(lmdata)
+nrow(excluded_dead)
+nrow(excluded_cens)
+
+lmdata$trans30 <- ifelse(lmdata$trans == 1 & lmdata$wait <= landmark, 1, 0)
+
+table(lmdata$trans30, useNA = "ifany")
+
+table(lmdata$trans30)
+sum(lmdata$trans30 == 1)
+
+# only patients still alive and under observation at day 30 were included. 
+# Patients who had died by day 30 were excluded, and patients censored before or at day 30 were also excluded. 
+# At the new baseline, patients were classified according to whether they had already received transplantation by day 30.
+
+#   Q1-3A Draw Kaplan-Meier curves according to the transplantation status at the landmark time. 
+# What is the prognosis after two years on the waiting list? 
+#   Does this analysis suffer from immortal time bias?
+
+lmdata$time_lm <- lmdata$days - landmark
+lmdata$event <- ifelse(lmdata$death == 1, 1, 0)
+
+
+km_lm <- survfit(Surv(time_lm, event) ~ trans30, data = lmdata)
+par(mfrow=c(1,1))
+plot(km_lm, col = 1:2, lty = 1:2,
+    xlab = "Days since landmark (day 30)",
+    ylab = "Survival probability")
+legend("bottomleft", legend = levels(factor(lmdata$trans30)),
+       col = 1:2, lty = 1:2)
+ss700 <- summary(km_lm, times = 700)
+round(100 * ss700$surv)
+
+
+data.frame(
+  group = ss700$strata,
+  surv_percent = round(100 * ss700$surv)
+)
+
+# The landmark analysis is specifically designed to avoid the classic immortal time bias 
+# from classifying patients by future transplantation status.
+
+#   Q1-4. In a landmark analysis after 30 days, calculate the mortality hazard ratio 
+# comparing transplanted patients according to their transplantation status at the landmark time.
+
+cox_lm <- coxph(Surv(time_lm, event) ~ trans30, data = lmdata)
+summary(cox_lm)
+exp(cbind(HR = coef(cox_lm), confint(cox_lm)))
+
+# The HR from this model compares mortality after day 30 between:
+# - patients transplanted by day 30
+# - patients not transplanted by day 30
+# among patients alive and still observed at day 30.
+
+# Q1-5A. Can you think about any potential problems in interpreting the 
+# “effect” of heart transplant on survival in the landmark analysis? 
+#   Think in terms of selection effects? Who are in the groups at day 30? 
+#   Who are excluded? 
+#   Are there other aspects to that might need consideration here?
+
+
+# The landmark analysis is still subject to selection bias. 
+# Only patients alive and under follow-up at day 30 are included, 
+# while those who died or were censored earlier are excluded. 
+# Thus, the groups compared at day 30 are selected survivors and may differ 
+# systematically in disease severity, transplant eligibility, and other prognostic factors. 
+# The estimated association therefore cannot be interpreted straightforwardly 
+# as a causal effect of heart transplantation.

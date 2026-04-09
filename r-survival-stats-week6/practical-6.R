fe37b44b687774e4b441caba07dfc4cffe2ffb2d
Frederik <kaae-2@hotmail.com>
2026-04-09 10:41:23 +0200

    Enable code and add melanoma prep exercises
    Uncomment R commands and run examples in practical-6.R (survival,
    prodlim, riskRegression workflows). Add prep-6.R with melanoma data
    processing, variable recoding and three prep questions.

--- r-survival-stats-week6/practical-6.R
+++ r-survival-stats-week6/practical-6.R
@@ -12,14 +12,15 @@
 # size tumour size (<=20mm, 20mm-50mm, >50 mm)
 # log2nodes number of positive lymph nodes, log2-transformed
 # To load the data into a data frame called bca in R write
-# bca <- read.csv("http://biostat.ku.dk/frank/data/bca-subset.csv")
-# bca$chemo <- factor(bca$chemo)
+bca <- read.csv("http://biostat.ku.dk/frank/data/bca-subset.csv")
+bca$chemo <- factor(bca$chemo)
 # 1. Plot the (event-free) survival curves for patients who did and did not receive chemotherapy,
 # respectively. What is the survival difference at 2000 days after surgery?
-# library(survival)
-# library(prodlim)
-# pl <- prodlim(Surv(time,event)~chemo, data=bca)
-# plot(pl)Time
+library(survival)
+library(prodlim)
+pl <- prodlim(Surv(time,event)~chemo, data=bca)
+plot(pl)
+# Time
 # Survival probability
 # 0 1000 2000 3000 4000 5000 6000
 # 0 % 50 % 100 %
@@ -28,7 +29,7 @@
 # chemo
 # 0
 # 1
-# summary(pl, times=2000)
+summary(pl, times=2000)
 # ## chemo time n.risk n.event n.lost surv se.surv lower upper
 # ## 1 0 2000 23 0 0 0.224 0.0394 0.147 0.302
 # ## 2 1 2000 210 0 0 0.504 0.0235 0.458 0.550
@@ -37,7 +38,7 @@
 # typical “Table 1”, with the three columns chemotherapy yes and no and total. Reflect on
 # any differences between groups. You may want to use the function univariateTable from the
 # package Publish
-# library(Publish)
+library(Publish)
 # univariateTable(chemo~Q(age)+Q(log2nodes)+size, data=bca)
 # ## Variable Level chemo = 0 (n=116) chemo = 1 (n=461) Total (n=577)
 # ## 1 age median [iqr] 44 [39.8, 48.0] 43 [38, 47] 43 [38, 47]
@@ -54,9 +55,9 @@
 # 3. Now consider also tumour size. Predict the probability to be event-free after 2000 days for
 # all combinations of chemotherapy and size groups (estimate 6 numbers)
 # There are multiple ways to do this, for example, you can fit 6 Kaplan-Meier curves with prodlim
-# pla <- prodlim(Surv(time,event)~chemo+size, data=bca)
+pla <- prodlim(Surv(time,event)~chemo+size, data=bca)
 # and use summary() to extract the probabilities
-# summary(pla, times=2000)
+summary(pla, times=2000)
 # ## chemo size time n.risk n.event n.lost surv se.surv lower upper
 # ## 1 0 20-50 2000 13 0 0 0.213 0.0481 0.118 0.307
 # ## 2 0 <=20 2000 7 0 0 0.338 0.1035 0.135 0.541
@@ -67,8 +68,8 @@
 # You can just use the numbers from above and skip to the next question. For those interested in R, another
 # way to get the same numbers is to use the predict function
 # ## chemo=1
-# nd1 <- data.frame(expand.grid(chemo=factor(1), size=c("<=20",">50","20-50")))
-# predict(pla, newdata=nd1, times=2000)
+nd1 <- data.frame(expand.grid(chemo=factor(1), size=c("<=20",">50","20-50")))
+predict(pla, newdata=nd1, times=2000)
 # ## $`chemo=1, size=<=20`
 # ## [1] 0.6191715
 # ##
@@ -78,8 +79,8 @@
 # ## $`chemo=1, size=20-50`
 # ## [1] 0.4641005
 # ## chemo=0
-# nd0 <- data.frame(expand.grid(chemo=factor(0), size=c("<=20",">50","20-50")))
-# predict(pla, newdata=nd0, times=2000)
+nd0 <- data.frame(expand.grid(chemo=factor(0), size=c("<=20",">50","20-50")))
+predict(pla, newdata=nd0, times=2000)
 # 2
 # ## $`chemo=0, size=<=20`
 # ## [1] 0.3380682
@@ -91,14 +92,14 @@
 # ## [1] 0.2125823
 # You can also use a Cox model stratified for both chemo and size and predict the probabilities with the
 # function predictRisk from the riskRegression package
-# library(riskRegression)
+library(riskRegression)
 # ## riskRegression version 2025.09.17
 # ##
 # ## Attaching package: 'riskRegression'
 # ## The following object is masked from 'package:mets':
 # ##
 # ## predictRisk
-# fit.cox <- coxph(Surv(time,event)~ strata(chemo)+strata(size),x=TRUE,data=bca)
+fit.cox <- coxph(Surv(time,event)~ strata(chemo)+strata(size),x=TRUE,data=bca)
 # 1-predictRisk(fit.cox, newdata=nd1, time=2000)
 # ## [,1]
 # ## [1,] 0.6201991
@@ -113,8 +114,8 @@
 # as fitting six Kaplan-Meier estimators.
 # 4. How many patients are there in each of the tumour size groups?
 # Numbers per group and total sample size
-# ngr <- table(bca$size)
-# n <- sum(ngr)
+ngr <- table(bca$size)
+n <- sum(ngr)
 # ngr
 # ##
 # ## <=20 >50 20-50
@@ -125,15 +126,21 @@
 # calculate the weighted average of the predicted survival at 2000 days, where each tumour
 # size group has weight according to it’s proportion in the data set. This can be achieved by
 # multiplying the estimated survival for the group with the number of patients in the group
-# 3
 # and summing over all three chemo=1 estimates and finally dividing by the total sample size.
 # You may do this “manually” by copy+paste (it can also be done directly in R). Redo the
 # procedure for the no chemotherapy group. Look at the difference between the weighted
 # averages. Compare this to the estimates and difference from question 1. Interpret.
+weighted.mean(unlist(predict(pla, newdata = nd1, times = 2000)), ngr)
+weighted.mean(unlist(predict(pla, newdata = nd0, times = 2000)), ngr)
+# [1] 0.4957646
+# [1] 0.2493604
+# 3
+
+
 # 6. Compare your the standardized estimates to those from the ate function of the riskRegression
 # package
-# fit.cox1 <- coxph(Surv(time,event)~ strata(chemo)+strata(size),x=TRUE,data=bca)
-# ate1 <- ate(fit.cox1, data = bca, treatment = "chemo", times = 2000, se = TRUE)
+fit.cox1 <- coxph(Surv(time,event)~ strata(chemo)+strata(size),x=TRUE,data=bca)
+ate1 <- ate(fit.cox1, data = bca, treatment = "chemo", times = 2000, se = TRUE)
 # ## Input variables
 # ## - Treatment : chemo (2 levels: "0" "1")
 # ## - Event : event (cause: TRUE, censoring: 0)
@@ -152,7 +159,7 @@
 # ## - Point estimation: done
 # ## - Decomposition iid: done
 # ## - Confidence intervals: done
-# summary(ate1)
+summary(ate1)
 # ## Average treatment effect
 # ##
 # ## - Treatment : chemo (2 levels: "0" "1")
@@ -190,8 +197,8 @@
 # survival difference (ATE) using age, log2nodes and size based on a Cox model. Interpret the
 # result. Do you think that the assumption of no unmeasured confounders is fulfilled?
 # We can estimate the ATE with the function ate from the package riskRegression
-# fit.cox2 <- coxph(Surv(time,event)~ chemo+age+size+log2nodes,x=TRUE,data=bca)
-# summary(ate(fit.cox2, data = bca, treatment = "chemo", times = 2000, se = TRUE))
+fit.cox2 <- coxph(Surv(time,event)~ chemo+age+size+log2nodes,x=TRUE,data=bca)
+summary(ate(fit.cox2, data = bca, treatment = "chemo", times = 2000, se = TRUE))
 # ## Input variables
 # ## - Treatment : chemo (2 levels: "0" "1")
 # ## - Event : event (cause: TRUE, censoring: 0)
@@ -244,8 +251,8 @@
 # ## ci : pointwise confidence intervals
 # ## p.value : (unadjusted) p-value
 # or with survivalG function from the mets package
-# fit.ph <- phreg(Surv(time,event)~ chemo+age+size+log2nodes,x=TRUE,data=bca)
-# summary(survivalG(fit.ph, time = 2000, data = bca))
+fit.ph <- phreg(Surv(time,event)~ chemo+age+size+log2nodes,x=TRUE,data=bca)
+summary(survivalG(fit.ph, time = 2000, data = bca))
 # ## G-estimator :
 # ## Estimate Std.Err 2.5% 97.5% P-value
 # ## risk0 0.6984 0.04076 0.6185 0.7783 8.208e-66
@@ -276,8 +283,8 @@
 # ## 1.569629 1.198693 2.055350
 # 8. Compare this to the non-standardized difference (this corresponds to the difference of the
 # unadjusted survival curves from question 1).
-# fit.cox0 <- coxph(Surv(time,event)~ strata(chemo),x=TRUE,data=bca)
-# ate0 <- ate(fit.cox0, data = bca, treatment = "chemo", times = 2000, se = TRUE)
+fit.cox0 <- coxph(Surv(time,event)~ strata(chemo),x=TRUE,data=bca)
+ate0 <- ate(fit.cox0, data = bca, treatment = "chemo", times = 2000, se = TRUE)
 # ## Input variables
 # ## - Treatment : chemo (2 levels: "0" "1")
 # ## - Event : event (cause: TRUE, censoring: 0)
@@ -297,7 +304,7 @@
 # ## - Point estimation: done
 # ## - Decomposition iid: done
 # ## - Confidence intervals: done
-# summary(ate0)
+summary(ate0)
 # ## Average treatment effect
 # ##
 # ## - Treatment : chemo (2 levels: "0" "1")
--- r-survival-stats-week6/prep-6.R
+++ r-survival-stats-week6/prep-6.R
@@ -1,2 +1,29 @@
-# Prep exercises for this week
-# TODO: add your prep tasks here.
+# Malignant melanoma
+# In this exercise we will look at the malignant melanoma data from Odense University Hospital again. In the period 1962-77, 205 patients had their tumour removed and were followed until 1977. At the end of 1977:
+# • 57 died of malignant melanoma (status=1) • 134 were still alive (status=2) • 14 died from other causes (status=3)
+# The variables in the data set are
+#     no Patient code
+#     status Survival status. 1: dead from melanoma, 2: alive, 3: dead from other cause
+#     days Survival time
+#     ulc Ulceration, 1: present, 0: absent
+#     thick Tumour thickness (1/100 mm)
+#     sex 0: female, 1: male
+# Purpose: Study effect of sex, age, thickness of tumor and ulceration on survival.
+# Reading data and recoding the censorings as 0 and making dead variable.
+library(mets)
+
+data(melanoma)
+
+melanoma <- dtransform(melanoma,status=0,status==2)
+melanoma <- dtransform(melanoma,dead=status!=0)
+head(melanoma)
+
+melanoma  <- dfactor(melanoma,fulc~ulc,labels=c("ulc-not-present","ulc-present"))
+dtable(melanoma,~fulc+ulc)
+# Answer the following questions and then answer the questions in the quiz
+
+# 1) Considering overall death, compute how many days you loose up to 5 years, by computing the restricted residual mean at 5 years in the two groups. How many days do you loose more if you have ulceration.
+
+# 2) We now wish to see how the "time lost" decompose on the two causes, malignant-melanoma death and other deaths. Compute how much of the time lost is due to malignant-melanoma deaths. What percentage of the time lost for the ulceration group is due to malignant melanoma deaths ?
+
+# 3) We finally wish to express how the ulceration changes the survival at 5 years. We will do this by computing a standardized survival estimate for ulceration. Do this by using a Cox model adjusting for thick(ness) and sex. You may use the surivalG function for this. What is the standardized risk difference due to ulceration ?
